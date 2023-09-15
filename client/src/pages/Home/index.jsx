af94283627a11e0a0fa6ec0ae8c49b1d318220fb
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-15 20:08:35 +0530

    feat: created typography component

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import { stylesConfig } from "../../utils/functions";
+import Typography from "../../library/Typography";
 
 const classes = stylesConfig(styles, "home");
 
 const HomePage = () => {
 	return (
 		<main className={classes("")}>
-			<section className={classes("-hero")}></section>
+			<section className={classes("-hero")}>
+				<Typography size="lg">
+					Hello, <span className="text--primary">world</span>!
+				</Typography>
+			</section>
 		</main>
 	);
 };
