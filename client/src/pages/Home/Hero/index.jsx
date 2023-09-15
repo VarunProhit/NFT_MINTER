380d7b95f5f717cb0afefd04cea2bc674ed36390
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-15 23:35:18 +0530

    refactor: restructured the component tree

--- client/src/pages/Home/Hero/index.jsx
+++ client/src/pages/Home/Hero/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Typography from "../../library/Typography";
-import { stylesConfig } from "../../utils/functions";
+import Typography from "../../../library/Typography";
+import { stylesConfig } from "../../../utils/functions";
 import styles from "./styles.module.scss";
 
 const classes = stylesConfig(styles, "home-hero");
