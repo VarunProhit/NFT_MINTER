4e3dbc18bd2751f20b21eda779834136ca46603d
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-15 21:00:12 +0530

    feat: created hero section

---
+++ client/src/pages/Home/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Typography from "../../library/Typography";
+import { stylesConfig } from "../../utils/functions";
+import styles from "./styles.module.scss";
+
+const classes = stylesConfig(styles, "home-hero");
+
+const HomeHero = () => {
+	return (
+		<section className={classes("")}>
+			<Typography
+				type="heading"
+				variant="display"
+				className={classes("-title")}
+			>
+				NFT Marketplace
+			</Typography>
+			<button
+				className={classes("-btn")}
+				type="button"
+				onClick={() => {
+					window.scroll(0, window.innerHeight);
+				}}
+			>
+				<span className={classes("-btn-arrow")} />
+			</button>
+		</section>
+	);
+};
+
+export default HomeHero;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,18 +1,14 @@
 import React from "react";
-import styles from "./styles.module.scss";
+import Hero from "./Hero";
 import { stylesConfig } from "../../utils/functions";
-import Typography from "../../library/Typography";
+import styles from "./styles.module.scss";
 
 const classes = stylesConfig(styles, "home");
 
 const HomePage = () => {
 	return (
 		<main className={classes("")}>
-			<section className={classes("-hero")}>
-				<Typography size="lg">
-					Hello, <span className="text--primary">world</span>!
-				</Typography>
-			</section>
+			<Hero />
 		</main>
 	);
 };
