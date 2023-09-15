6c96418160adafbcae31d5749a905385f57a2714
Akshat Mittal <akshatmittal2506@gmail.com>
2023-09-15 23:32:29 +0530

    feat: created footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./styles.scss";
 import HomePage from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<>
 			<HomePage />
+			<Footer />
 		</>
 	);
 }
---
+++ client/src/constants/footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FiGithub, FiInstagram, FiMail, FiTwitter } from "react-icons/fi";
+
+export const socials = [
+	{
+		name: "Email",
+		icon: <FiMail />,
+		link: "mailto:akshatmittal2506@gmail.com",
+	},
+	{
+		name: "Twitter",
+		icon: <FiTwitter />,
+		link: "https://twitter.com/akshatmittal61",
+	},
+	{
+		name: "Github",
+		icon: <FiGithub />,
+		link: "https://github.com/akshatmittal61",
+	},
+	{
+		name: "Instagram",
+		icon: <FiInstagram />,
+		link: "https://www.instagram.com/akshatmittal61/",
+	},
+];
