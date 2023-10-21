30123fa179547dee8a3e121bd73cdd2eaeacd211
Akshat Mittal <akshatmittal2506@gmail.com>
2023-10-21 10:53:16 +0530

    feat: typography compoonent

--- src/library/Typography/index.jsx
+++ src/library/Typography/index.jsx
@@ -1,27 +1,70 @@
 import React from "react";
-import styles from "./styles.module.scss";
+import PropTypes from "prop-types";
 import { stylesConfig } from "../../utils/functions";
+import styles from "./styles.module.scss";
 
-const classes = stylesConfig(styles, "typography-");
+const classes = stylesConfig(styles, "typography");
 
 const Typography = ({
-	type = "body",
-	variant = "medium",
-	format = "regular",
 	children,
-	className,
+	family = "montserrat",
+	size = "md",
+	weight = "regular",
+	as = "span",
+	className = "",
 	...rest
 }) => {
+	const Component = as || "span";
+
 	return (
-		<span
+		<Component
 			className={
-				classes([type, variant, format].join("-")) + " " + className
+				classes("", `--${family}-${size}-${weight}`) + ` ${className}`
 			}
 			{...rest}
 		>
 			{children}
-		</span>
+		</Component>
 	);
 };
 
 export default Typography;
+
+/* 
+import React from "react";
+
+// All the font definitions can be found in @/styles/config/_typography.scss
+
+export type FontFamily = "montserrat" | "source-sans";
+
+export type FontSize =
+	| "xxs"
+	| "xs"
+	| "sm"
+	| "md"
+	| "lg"
+	| "xl"
+	| "xxl"
+	| "xxxl";
+
+export type FontWeight = "regular" | "medium" | "semi-bold" | "bold";
+
+export interface TypographyProps extends React.HTMLAttributes<HTMLElement> {
+	family?: FontFamily;
+	size?: FontSize;
+	weight?: FontWeight;
+	children: React.ReactNode;
+	as?: React.ElementType;
+}
+
+ */
+// create prop types
+
+Typography.propTypes = {
+	family: PropTypes.oneOf(["montserrat", "source-sans"]),
+	size: PropTypes.oneOf(["xxs", "xs", "sm", "md", "lg", "xl", "xxl", "xxxl"]),
+	weight: PropTypes.oneOf(["regular", "medium", "semi-bold", "bold"]),
+	children: PropTypes.node.isRequired,
+	as: PropTypes.elementType,
+	className: PropTypes.string,
+};
