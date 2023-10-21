ee75d8b20539b23d44fbf553e4807d9f6fb7228c
Akshat Mittal <akshatmittal2506@gmail.com>
2023-10-21 19:47:55 +0530

    feat: added header in hero component

--- src/pages/Home/Hero/index.jsx
+++ src/pages/Home/Hero/index.jsx
@@ -15,24 +15,44 @@ const HomeHero = () => {
 				backgroundImage: "url(/images/bg.gif)",
 			}}
 		>
-			<div className={classes("-content")}>
-				<Typography as="h1" className={classes("-title")}>
-					Discover and collect extraordinary NFTs
-				</Typography>
-				<Typography as="p" className={classes("-subtitle")} size="md">
-					On NFT Marketplace you can buy, sell, and explore digital
-					goods secured with blockchain technology.
-				</Typography>
-				<Button
-					icon={<img src={metamaskIcon} alt="metamask" />}
-					className={classes("-button")}
-					size="large"
-				>
-					Connect your wallet
-				</Button>
+			<div className={classes("-header")}>
+				<img src="favicon.png" alt="logo" />
+				<nav>
+					<ul>
+						<li>Home</li>
+						<li>Marketplace</li>
+						<li>Create</li>
+						<li>Your Profile</li>
+					</ul>
+				</nav>
+				<Button size="small">Explore</Button>
 			</div>
-			<div className={classes("-graphic")}>
-				<img src="/images/cat.png" alt="Robot" />
+			<div className={classes("-body")}>
+				<div className={classes("-content")}>
+					<Typography as="h1" className={classes("-title")}>
+						Discover and collect extraordinary NFTs
+					</Typography>
+					<Typography
+						as="p"
+						className={classes("-subtitle")}
+						size="md"
+					>
+						On NFT Marketplace you can buy, sell, and explore
+						digital goods secured with blockchain technology.
+					</Typography>
+					<Button
+						icon={<img src={metamaskIcon} alt="metamask" />}
+						className={classes("-button")}
+						size="large"
+					>
+						Connect your wallet
+					</Button>
+					<span className="empty"></span>
+					<span className="empty"></span>
+				</div>
+				<div className={classes("-graphic")}>
+					<img src="/images/cat.png" alt="Robot" />
+				</div>
 			</div>
 		</section>
 	);
